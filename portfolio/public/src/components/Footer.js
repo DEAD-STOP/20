6e33407e5caad3d1e2361f4c2af38bcc75233102
DEAD-STOP <arstop98@gmail.com>
2022-09-18 22:30:41 -0500

    commitment

---
+++ portfolio/public/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './style.css';
+
+function Footer() {
+  return (
+<div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">More</h5>
+              <a href="#" class="grey-text text-lighten-4">To be added</a>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Links</h5>
+              <ul>
+                <li><a class="grey-text text-lighten-5" href="#!">Nothing here</a></li>
+              </ul>
+            </div>
+          </div>
+  );
+}
+
+export default Footer;
--- portfolio/public/src/components/component.js
+++ portfolio/public/src/components/component.js
@@ -1,1 +1,36 @@
-// nothing 'ere yet
+import React, { useState } from 'react';
+import './style.css';
+
+function Welcome() {
+
+  return (
+    <div>
+          <nav class="#80cbc4 teal lighten-3 white-text nav-wrapper valign-wrapper z-depth-1">
+        <h3>Hello, this is my Portfolio.</h3>
+    </nav>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col l4 s12 ">
+                    <p><a href=""></a></p>
+                </div>
+                <div class="col l4 s12 offset-l2">
+                    <p><a href=""></a></p>
+                </div>
+            </div>
+            <div class="row">
+                {}
+            </div>
+
+        </div>
+    </div>
+    <footer class="page-footer #80cbc4 teal lighten-3 valign-wrapper">
+        <div class="container">
+                <Footer />
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Welcome;
